5168b40d00bbdd06d6b24b3bd6d319f5b5d78282
Dan Herrera <sonarius@shew.io>
2019-11-04 17:48:10 -0500

    Fix up broken fish config

--- roles/workstation/files/.config/fish/config.fish
+++ roles/workstation/files/.config/fish/config.fish
@@ -9,6 +9,9 @@ source /usr/local/share/chruby/auto.fish
 set -x GOPATH $HOME/.go
 set PATH $GOPATH/bin $PATH
 
+# Cargo Bin Path
+set PATH $HOME/.cargo/bin $PATH
+
 # Starship Prompt
 eval (starship init fish)
 
@@ -18,14 +21,9 @@ alias vim="nvim"
 # Tmux Helper Func for Making Teamocil Layouts
 alias tmux-layout='tmux list-windows -F "#{window_active} #{window_layout}" | grep "^1" | cut -d " " -f 2'
 
-# Source secrets ,':3
-if test -f $HOME/.config/fish/secret.fish
-  source $HOME/.config/fish/secret.fish
-end
-
 # Create/Attach to main tmux session on startup
 if test -z $TMUX
-  session=(tmux list-sessions 2> /dev/null | sort | head -n1 | cut -d: -f1)
+  set session (tmux list-sessions 2> /dev/null | sort | head -n1 | cut -d: -f1)
   if test -z $session
     tmux new -s 0
   else
